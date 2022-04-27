178e7a5d620c763687130e4a76e6c843279b1213
Nig3l <nig3lpro@gmail.com>
2022-04-27 21:53:12 +0200

    fix(signature): copy of template

--- tools/signature_generator/js/signature_generator.js
+++ tools/signature_generator/js/signature_generator.js
@@ -14,19 +14,30 @@ function generate_signature() {
     document.getElementById("copy-button").style.display = 'block'
 }
 
+function add_to_clipboard(container_node) {
+
+    function listener(e) {
+        e.clipboardData.setData("text/html", container_node.innerHTML);
+        e.preventDefault();
+    }
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+}
+
 function copy_signature() {
     let copy_button = document.getElementById("copy-button")
 
     copy_button.setAttribute('aria-busy', true)
     copy_button.innerHTML = ''
-    
+
     setTimeout(function () {
         let template = document.getElementById('sign-template')
-        navigator.clipboard.writeText(template.innerHTML);
+        add_to_clipboard(template)
         copy_button.setAttribute('aria-busy', false)
         copy_button.innerHTML = '<i class="fa-solid fa-clipboard"></i> &nbsp; COPIED !'
     }, 2000);
-    
+
     setTimeout(function () {
         copy_button.innerHTML = '<i class="fa-solid fa-clipboard"></i> &nbsp; COPY !'
     }, 7000);
